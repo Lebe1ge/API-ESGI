fb3de21901e1c5a863428299e712c8a79c8d4826
bobby <jeremy.felicite@gmail.com>
2017-03-17 13:20:55 +0100

    add ensureAuthenticated route project

--- routes/projects.js
+++ routes/projects.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   let router = new Router()
 
   router.post('/',
-    app.middlewares.ensureAuthenticated, 
+    app.middlewares.ensureAuthenticated,
     app.middlewares.bodyParser.json(),
     app.middlewares.ensureFields('name'),
     app.actions.projects.create
@@ -12,14 +12,21 @@ module.exports = (app) => {
 
   router.get('/', app.actions.projects.list)
 
-  router.get('/:id', app.actions.projects.show)
+  router.get('/:id',
+    app.middlewares.ensureAuthenticated,
+    app.actions.projects.show
+  )
 
   router.put('/',
+    app.middlewares.ensureAuthenticated,
     app.middlewares.bodyParser.json(),
     app.actions.projects.update
   )
 
-  router.delete('/:id', app.actions.projects.remove)
+  router.delete('/:id',
+    app.middlewares.ensureAuthenticated,
+    app.actions.projects.remove
+  )
 
   return router;
 }
